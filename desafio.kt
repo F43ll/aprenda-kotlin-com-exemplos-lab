c33e4bb3a684cb82de085337bd5c6f2481a35c83
F43ll <96871783+F43ll@users.noreply.github.com>
2023-07-28 18:06:14 -0300

    Adicionei os usuários e conteúdos para testar o código

--- desafio.kt
+++ desafio.kt
@@ -4,19 +4,32 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, val: nivel:Nivel, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel:Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
         inscritos.addAll(usuario)
     }
 }
 
 fun main() {
-    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    val Drake = Usuario()
+    val Kendrick = Usuario()
+    val Rocky = Usuario()
+    val Wayne = Usuario()
+
+    val Materia1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val Materia2 = ConteudoEducacional("Estruturas de Dados", 120)
+    val Materia3 = ConteudoEducacional("Arquitetura Mobile", 80)
+
+    val formacao = Formacao("Mobile Developer", Nivel.AVANCADO, listOf(Materia1, Materia2, Materia3))
+    
+    formacao.matricular(Drake)
+    formacao.matricular(Kendrick)
+    formacao.matricular(Rocky)
+    formacao.matricular(Wayne)
+
+    }
